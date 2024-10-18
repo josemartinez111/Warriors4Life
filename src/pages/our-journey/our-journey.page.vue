ff6467b1dc5b744a009aa2fb4637537225d6aa8e
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2024-10-18 01:27:31 -0400

    ⚡pushing -> `pushing changes 10-18-24` | pushing PR..

--- src/pages/our-journey/our-journey.page.vue
+++ src/pages/our-journey/our-journey.page.vue
@@ -5,38 +5,17 @@
                         SCRIPT-SETUP
 --------------------------------------------------------- -->
 <script setup lang="ts">
-import { ref } from 'vue';
-
 // ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
 import Layout from '../../components/Layout.vue';
-import { UseCarouselImages } from '../../composables/UseCarouselImages.ts';
 import { UseColorStore } from '../../stores';
 import {
-  LeftTextSection,
   CarouselSection,
-} from '../../components/index.ts';
+  LeftTextSection,
+  StackedImagesSection,
+} from '../../components';
 // ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
 
-const stackImageName = ref<string>();
-
 const colorStore = UseColorStore();
-// Use the composable to get images
-const { images } = UseCarouselImages();
-// This will hold the current images in the stack
-const currentImages = ref(images);
-
-// Function to handle moving the top image to the back
-const moveTopImageToBack = () => {
-  if (currentImages.value.length <= 0) {
-    return;
-  }
-  
-  const topImage = currentImages.value.shift();
-  
-  if (topImage) {
-    currentImages.value.push(topImage); // Add the removed image to the end
-  }
-};
 </script>
 <!-- --------------------------------------------------------
                      <>MARKUP</>
@@ -67,43 +46,7 @@ const moveTopImageToBack = () => {
         </section>
         
         <!-- Section 2 (New Section Below Section 1) -->
-<section class="bg-white shadow-accent">
-  <!-- Container for the Stack of Images -->
-  <div class="w-full lg:w-5/12 px-4 mb-6 lg:mb-0 mx-auto">
-    <!-- Image Stack Card -->
-    <div class="relative flex flex-col min-w-0 break-words w-full mb-4 shadow-lg rounded-lg">
-      <!-- DaisyUI Stack of Images with transition -->
-      <div class="stack pt-8 cursor-pointer bg-white rounded-t-lg overflow-hidden">
-        <!-- Loop through images, click moves the top image to the back -->
-        <img
-          v-for="(image, index) in currentImages"
-          :key="image.id"
-          :src="image.src"
-          class="forloop-images transition-transform duration-500 ease-in-out transform"
-          :class="{
-            'translate-x-full': index === 0,
-            'translate-x-[-9%] translate-y-10 scale-85 opacity-70': index !== 1,
-            'translate-x-[-7%] translate-y-10 scale-90 opacity-65': index !== 2,
-            'translate-y-30 scale-95 opacity-50': index < 2,
-            'sm:translate-x-0 sm:translate-y-5 sm:scale-70 sm:opacity-80': index === 1,
-            'sm:translate-x-0 sm:translate-y-6 sm:scale-75 sm:opacity-70': index !== 2,
-            'sm:translate-y-10 sm:scale-80 sm:opacity-60': index < 2
-          }"
-          :alt="image.alt"
-          @click="moveTopImageToBack"
-        />
-      </div>
-      
-      <!-- Bottom section for the title -->
-      <blockquote class="p-6 rounded-b-lg">
-        <!-- Title for the stack images -->
-        <h2 class="px-2 sm:px-3 py-1 sm:py-2 md:text-4xl text-xs font-semibold tracking-wider uppercase dark:bg-rose-800">
-          {{ stackImageName = 'First Stack' }}
-        </h2>
-      </blockquote>
-    </div>
-  </div>
-</section>
+        <StackedImagesSection />
       </div>
    </Layout>
 </template>
@@ -111,10 +54,6 @@ const moveTopImageToBack = () => {
                             STYLES
 --------------------------------------------------------- -->
 <style scoped lang="postcss">
-
-.forloop-images {
-  @apply rounded w-[28rem] h-[38rem] pb-20 pt-16 object-cover shadow-accent;
-}
 
 .carousel-content {
   @apply container mx-auto z-10 text-left text-white h-full
