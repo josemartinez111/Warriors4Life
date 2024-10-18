d0568564223eeeb16569fc296f15dbdef19eb3f7
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2024-10-18 15:36:46 -0400

    ⚡pushing -> `pushing changes 10-18-24` | pushing PR..

--- src/components/page-components/our-journey/sections/StackedImagesSection.vue
+++ src/components/page-components/our-journey/sections/StackedImagesSection.vue
@@ -1,5 +1,5 @@
 <!-- ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
-                       COMPONENT_PATH
+Components > PageComponents > OurJourney > Sections > StackedImagesSection
 ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞ -->
 <!-- --------------------------------------------------------
                         SCRIPT-SETUP
@@ -20,17 +20,14 @@ const CARD_NAMES: string[] = [
                      <>MARKUP</>
 --------------------------------------------------------- -->
 <template>
-  <section class="bg-white w-full shadow-accent">
-    <!-- Container for the Stack of Images -->
-    <div class="flex flex-row justify-between w-full px-4 space-x-6">
-      <!-- DaisyUI Stack of Images with transition/Image Stack Card Iteration -->
-      <template v-for="(cardName) in CARD_NAMES">
-        <StackedImagesCard
-          :card-name="cardName"
-          class="w-full lg:w-1/3"
-        />
-      </template>
-      <div class="pb-32" />
+  <section class="bg-white w-full shadow-accent md:py-28">
+    <div class="image-card">
+      <StackedImagesCard
+        v-for="cardName in CARD_NAMES"
+        :key="cardName"
+        :card-name="cardName"
+        class="w-full sm:w-1/3"
+      />
     </div>
   </section>
 </template>
@@ -38,9 +35,9 @@ const CARD_NAMES: string[] = [
                             STYLES
 --------------------------------------------------------- -->
 <style scoped lang="postcss">
-
-.carousel-item img {
-  @apply w-full h-full object-cover;
+.image-card {
+  @apply flex flex-col items-center sm:flex-row sm:items-stretch
+  justify-between w-full px-4 space-y-2 sm:space-y-0 sm:space-x-6;
 }
 </style>
 <!-- ---------------------------------------------------- -->
